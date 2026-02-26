28cd365daf5b3b9773d095e003cbb1579f6540f2
Omar Gallegos <omar.gallegos@thth.studio>
2026-02-25 22:35:02 -0600

    build: set up island architecture for independent components
    Removes Vite boilerplate code since there is no entry point App that
    controls the whole state of the DOM, rather each components get's
    rendered individually.

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,19 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react()],
+    server: {
+        cors: true,
+    },
+    build: {
+        outDir: 'dist',
+        emptyOutDir: true,
+        rollupOptions: {
+            input: 'src/main.tsx',
+            output: {
+                entryFileNames: 'main.js',
+                assetFileNames: 'main.[ext]',
+                format: 'es',
+            },
+        },
+    },
 })
